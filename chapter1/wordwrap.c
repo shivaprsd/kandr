c7344231a507dc57a4b4ded0186315f46adb72c3
ShivaPrasad <shiva.prasadv@yahoo.com>
2016-07-10 13:12:22 +0530

    fix the overflowing spaces bug
    Whenever spaces/tabs overflowed the column width, they were moved
    to the new line like any other character. But this created completely
    blank output lines additional to normal blank lines typed in using
    newline characters. Also, new lines of output began with empty spaces
    (which might be desirable in some cases) but found inappropriate for
    a word wrapping program. This fix removes both bugs.
    
    The program is also fully documented.

--- chapter1/wordwrap.c
+++ chapter1/wordwrap.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
-#define MAXWIDTH 10     /* maximum column width of lines */
-#define TABSTOP  8      /* column width of a tab */
+#define MAXWIDTH 20     /* maximum column size of lines */
+#define TABSTOP  8      /* column size of a tab */
 
 int getword(char word[]);
 int printword(char word[],int length, int cp);
+int printblank(char blank, int cp);
 int expandtab(int cp);
-int printblanks(char blanks, int cp);
 
 /* fold long lines of input into multiple shorter lines */
 main()
 {
-    int c, i, cp, len;
+    int c, cp, len;
     char word[MAXWIDTH];    /* save input words */
 
-for (i=0; i<MAXWIDTH; ++i) printf("%d",i); putchar('\n');
     cp = 0;             /* position of the char in the line */
     len = 0;            /* length of the next word */
     while ((len=getword(word)) > 0)
@@ -22,6 +21,8 @@ for (i=0; i<MAXWIDTH; ++i) printf("%d",i); putchar('\n');
     return 0;
 }
 
+/* getword: read a single word into s and return it's length
+ * if the word is longer than column size then it's wrapped mid-word */
 int getword(char s[])
 {
     int c, len;
@@ -37,37 +38,32 @@ int getword(char s[])
     return 0;
 }
 
+/* printword: print a word considering its length and cursor position;
+ * returns the new cursor position after printing */
 int printword(char s[], int len, int cp)
 {
     int i;
 
-    if (cp+len-1 > MAXWIDTH)
+    if (len > 1)        /* word contains atleast one non-blank character */
     {
-        putchar('\n');
-        cp = 0;
+        /* len-1 instead of len for ignoring the last char
+         * which is processed seperately using printblank */
+        if (cp+len-1 > MAXWIDTH)
+        {
+            putchar('\n');  /* wrap word if it overflows column size */
+            cp = 0;
+        }
+        for (i = 0; i < len-1; ++i, ++cp)   /* print the word */
+            putchar(s[i]);
     }
-    for (i = 0; i < len-1; ++i)
-        putchar(s[i]);
-    cp = printblanks(s[i], cp+len-1);
+    cp = printblank(s[len-1], cp);
     return cp;
 }
 
-int expandtab(int cp)
-{
-    int incr, i;
-
-    incr = TABSTOP - cp%TABSTOP;
-    if (cp+incr > MAXWIDTH)
-    {
-        putchar('\n');
-        return 0;
-    }
-    for (i = 0; i < incr; ++i)
-        putchar(' ');
-    return cp+incr;
-}
-
-int printblanks(char c, int cp)
+/* printblank: print the blank character considering the cursor position
+ * also handles non-blank characters of words that are wrapped mid-word;
+ * returns the new cursor position after printing */
+int printblank(char c, int cp)
 {
     if (c == '\n')
     {
@@ -76,13 +72,31 @@ int printblanks(char c, int cp)
     }
     else if (c == '\t')
         cp = expandtab(cp);
-    else
+    else if (c == ' ' && cp >= MAXWIDTH)    /* if no room for blank spaces, */ 
+        ;                                   /* do nothing! ignore it. */
+    else        /* it is either a space that has room, or a mid-word char */
     {
-        if (c == ' ')
-            putchar(' ');
-        else
-            putchar(c);
+        putchar(c);
         ++cp;
+    }
+    return cp;
+}
+
+/* expandtab: expand a tab to the next TABSTOP or column-end (whichever comes
+ * first); return the new cursor position after printing */
+int expandtab(int cp)
+{
+    int incr;
+
+    incr = TABSTOP - cp%TABSTOP;    /* spaces needed to reach next TABSTOP */
+    if (cp+incr > MAXWIDTH)         /* end of the column comes first */
+        incr = MAXWIDTH - cp;
+    /* expand the tab into spaces */
+    while (incr)
+    {
+        putchar(' ');
+        ++cp;
+        --incr;
     }
     return cp;
 }
