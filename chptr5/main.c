2a917e79de04a496ed3bbbc2204b666839194f70
ShivaPrasad <shiva.prasadv@yahoo.com>
2016-07-18 21:24:42 +0530

    complete exercise 5-4: strend

--- chptr5/main.c
+++ chptr5/main.c
@@ -9,5 +9,10 @@ main()
     char s2[] = "world!";
     str_cat(s1, s2);
     printf("%s\n", s1);
+    /* str_end */
+    if (str_end(s1, s2))
+        printf("%s found at the end of %s\n", s2, s1);
+    else
+        printf("%s is not at the end of %s\n", s2, s1);
     return 0;
 }
--- chptr5/str.c
+++ chptr5/str.c
@@ -4,10 +4,25 @@
 /* str_cat: concatenate t to end of s; assumes s is big enough */
 void str_cat(char *s, char *t)
 {
-    while (*s)        /* find end of s */
+    while (*s)          /* find end of s */
         s++;
     while ((*s++ = *t++))
         ;
+}
+
+/* str_end: return 1 if t occurs at the end of s, 0 otherwise */
+int str_end(char *s, char *t)
+{
+    char *d = t;        /* remember the beginning of t */
+
+    while (*s)          /* find end of s */
+        s++;
+    while (*t++)        /* move back s as many times as there are chars in t */
+        s--;
+    while (*d++ == *s++)    /* compare d (i.e. t) with the end of s */
+        if (!*d)            /* *d is \0, reached end of d */
+            return 1;
+    return 0;
 }
 
 #endif
